3b762f67429a0b3a0a1c20455c75ddfa2c1d4033
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-09-29 22:02:22 -0600

    Comment adding and category adding working so far. Razor pages not working.

--- hw-05-cleanup-emmanuel_hector-master/HW-03/Pages/Blogs/Category.cshtml.cs
+++ hw-05-cleanup-emmanuel_hector-master/HW-03/Pages/Blogs/Category.cshtml.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using HW_03.Models;
 
-namespace HW_03.Pages
+
+namespace Blog.Pages
 {
-    public class CategoryModel : PageModel
-    {
-        public void OnGet()
-        {
 
-        }
-    }
+
 }
